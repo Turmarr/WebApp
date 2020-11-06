c954be2163200d644137399de62e1a11f53a1bf5
Tuomas Rislakki <tuomas.rislakki@aalto.fi>
2020-11-06 14:47:51 +0200

    more advanced app

--- app.js
+++ app.js
@@ -1,46 +1,88 @@
+import { decode } from "https://deno.land/std@0.65.0/encoding/utf8.ts";
 import { serve } from "https://deno.land/std@0.65.0/http/server.ts";
+import { renderFile } from 'https://deno.land/x/dejs@0.8.0/mod.ts';
+import { Client } from "https://deno.land/x/postgres@v0.4.5/mod.ts";
 
-let port = 7777;
-
-if (Deno.args.length > 0) {
-    const lastArgument = Deno.args[Deno.args.length - 1];
+const DATABASE_URL = Deno.env.toObject().DATABASE_URL;
+const client = new Client(DATABASE_URL)
+;let port = 7777;
+if (Deno.args.length > 0) {  
+    const lastArgument = Deno.args[Deno.args.length - 1];  
     port = Number(lastArgument);
 }
 const server = serve({ port: port });
 
-const requestParams = (url) => {
-    let queryParams = '';
-    if (url.includes('?')) {
-        queryParams = url.slice(url.indexOf('?'));
+const executeQuery = async(query, ...args) => {
+    try {
+        await client.connect();
+        return await client.query(query, ...args);
+    } catch (e) {
+        console.log(e);
+    } finally {
+        await client.end();
+    }
+}
+
+const getNames = async() => {
+    const result = await executeQuery("SELECT id, name FROM names;");
+    if (result) {
+        return result.rowsOfObjects();
     }
 
-    return new URLSearchParams(queryParams);
+    return [];
+}
+
+const addName = async(request) => {
+    const body = decode(await Deno.readAll(request.body));
+    const params = new URLSearchParams(body);
+
+    const name = params.get('name');
+
+    executeQuery("INSERT INTO names (name) VALUES ($1);", name);
+}
+
+const deleteName = async(request) => {
+    console.log(`Delete name based on request url ${request.url}`);
+    const parts = request.url.split('/');
+    const id = parts[2];
+
+    executeQuery("DELETE FROM names WHERE id = $1;", id);
+};
+
+const redirectToNames = (request) => {
+    request.respond({
+        status: 303,
+        headers: new Headers({
+            'Location': '/names',
+        })
+    });
+};
+
+const handleGetNames = async(request) => {
+    const data = {
+        names: await getNames()
+    };
+    request.respond({ body: await renderFile('index.ejs', data) });
+};
+
+const handlePostNames = async(request) => {
+    await addName(request);
+    redirectToNames(request);
+}
+
+const handleDeleteNames = async(request) => {
+    await deleteName(request);
+    redirectToNames(request);
 }
 
 for await (const request of server) {
-    const params = requestParams(request.url);
-
-    let res = '';
-    if (params.has('operation') && params.has('number1') && params.has('number2')) {
-        
-        const op = params.get('operation');
-        const n1 = Number(params.get('number1'));
-        const n2 = Number(params.get('number2'));
-        if (op === 'sum') {
-            res = n1 + n2;
-        } else if (op === 'difference') {
-            res = n1 - n2;
-        } else if (op === 'product') {
-            res = n1 * n2;
-        } else if (op === 'quotient') {
-            res = n1 / n2;
-        } else {
-            res = 'Invalid parameters.';
-        }
-
+    if (request.method === 'GET' && request.url === '/names') {
+        await handleGetNames(request);
+    } else if (request.method === 'POST' && request.url === '/names') {
+        await handlePostNames(request);
+    } else if (request.method === 'POST' && request.url.includes('delete')) {
+        await handleDeleteNames(request);
     } else {
-        res = 'Invalid parameters.';
+        redirectToNames(request);
     }
-
-    request.respond({ body: `${res}` });
 }
