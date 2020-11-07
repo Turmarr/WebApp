7d9ed7044e7804c3d03b2ecaf6459f320aedb787
Tuomas Rislakki <tuomas.rislakki@aalto.fi>
2020-11-07 18:08:43 +0200

    message app

--- app.js
+++ app.js
@@ -3,86 +3,72 @@ import { serve } from "https://deno.land/std@0.65.0/http/server.ts";
 import { renderFile } from 'https://deno.land/x/dejs@0.8.0/mod.ts';
 import { Client } from "https://deno.land/x/postgres@v0.4.5/mod.ts";
 
-const DATABASE_URL = Deno.env.toObject().DATABASE_URL;
-const client = new Client(DATABASE_URL)
-;let port = 7777;
-if (Deno.args.length > 0) {  
-    const lastArgument = Deno.args[Deno.args.length - 1];  
-    port = Number(lastArgument);
-}
-const server = serve({ port: port });
+const client = new Client({
+    
+});
+
+const server = serve({ port: 7777 });
 
 const executeQuery = async(query, ...args) => {
+    let res = [];
     try {
         await client.connect();
-        return await client.query(query, ...args);
+        res = await client.query(query, ...args);
     } catch (e) {
         console.log(e);
     } finally {
         await client.end();
+        return res;
     }
 }
 
-const getNames = async() => {
-    const result = await executeQuery("SELECT id, name FROM names;");
+const getMessages = async(request) => {
+    const result = await executeQuery("SELECT * FROM messages ORDER BY id DESC LIMIT 5;");
     if (result) {
         return result.rowsOfObjects();
     }
-
     return [];
 }
 
-const addName = async(request) => {
+const postMessage = async(request) => {
     const body = decode(await Deno.readAll(request.body));
     const params = new URLSearchParams(body);
 
-    const name = params.get('name');
-
-    executeQuery("INSERT INTO names (name) VALUES ($1);", name);
+    const sender = params.get('sender');
+    const message = params.get('message');
+    //console.log('before');
+    await executeQuery("INSERT INTO messages (sender, message) VALUES ($1, $2)", sender, message);
+    //console.log('after');
 }
 
-const deleteName = async(request) => {
-    console.log(`Delete name based on request url ${request.url}`);
-    const parts = request.url.split('/');
-    const id = parts[2];
-
-    executeQuery("DELETE FROM names WHERE id = $1;", id);
-};
-
-const redirectToNames = (request) => {
+const redirectToMessages = (request) => {
     request.respond({
         status: 303,
         headers: new Headers({
-            'Location': '/names',
+            'Location': '/',
         })
     });
 };
 
-const handleGetNames = async(request) => {
+const handleGetMessages = async(request) => {
     const data = {
-        names: await getNames()
+        messages: await getMessages()
     };
-    request.respond({ body: await renderFile('index.ejs', data) });
-};
-
-const handlePostNames = async(request) => {
-    await addName(request);
-    redirectToNames(request);
+    request.respond({body: await renderFile('index.ejs',data) });
 }
 
-const handleDeleteNames = async(request) => {
-    await deleteName(request);
-    redirectToNames(request);
+const handlePostMessage = async(request) => {
+    await postMessage(request);
+    redirectToMessages(request);
+
 }
 
 for await (const request of server) {
-    if (request.method === 'GET' && request.url === '/names') {
-        await handleGetNames(request);
-    } else if (request.method === 'POST' && request.url === '/names') {
-        await handlePostNames(request);
-    } else if (request.method === 'POST' && request.url.includes('delete')) {
-        await handleDeleteNames(request);
+    if (request.method === 'GET' && request.url === '/') {
+        await handleGetMessages(request);
+    } else if (request.method === 'POST' && request.url === '/') {
+        await handlePostMessage(request);
     } else {
-        redirectToNames(request);
+        redirectToMessages(request);
     }
 }
